c25ec716e7289c86acdbd87fd1bed5edd3a78e16
grincon <grincon@sura.com.co>
2019-08-05 14:26:52 -0500

    cambios

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -10,192 +10,197 @@ export const SEMANAS=[
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana3.png',
         titulo: 'Semana 3',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana4.png',
         titulo: 'Semana 4',
         desc: 'Siempre sabremos donde estás!'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana5.png',
         titulo: 'Semana 5',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana6.png',
         titulo: 'Semana 6',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana7.png',
         titulo: 'Semana 7',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana8.png',
         titulo: 'Semana 8',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana9.png',
         titulo: 'Semana 9',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana10.png',
         titulo: 'ESemana 10',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana11.png',
         titulo: 'Semana 11',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana12.png',
         titulo: 'Semana 12',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana13.png',
         titulo: 'Semana 13',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana14.png',
         titulo: 'Semana 14',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana15.png',
         titulo: 'Semana 15',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana16.png',
         titulo: 'Semana 16',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana17.png',
         titulo: 'Semana 17',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana18.png',
         titulo: 'Semana 18',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana19.png',
         titulo: 'Semana 19',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana20.png',
         titulo: 'Semana 20',
         desc: 'Siempre sabremos donde estás!'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana21.png',
         titulo: 'Semana 21',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana22.png',
         titulo: 'Semana 22',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana23.png',
         titulo: 'Semana 23',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana24.png',
         titulo: 'Semana 24',
         desc: 'Siempre sabremos donde estás!'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana25.png',
         titulo: 'Semana 25',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana26.png',
         titulo: 'Semana 26',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana27.png',
         titulo: 'Semana 27',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana28.png',
         titulo: 'Semana 28',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana29.png',
         titulo: 'Semana 29',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/music-player-2.svg',
+        img: '/assets/slides/semana30.png',
         titulo: 'Semana 30',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/calendar.svg',
+        img: '/assets/slides/semana31.png',
         titulo: 'Semana 31',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana32.png',
         titulo: 'Semana 32',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana33.png',
         titulo: 'Semana 33',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/music-player-2.svg',
+        img: '/assets/slides/semana34.png',
         titulo: 'Semana 34',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/calendar.svg',
+        img: '/assets/slides/semana35.png',
         titulo: 'Semana 35',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana36.png',
         titulo: 'Semana 36',
         desc: 'Siempre sabremos donde estás!'
       }, {
-        img: '/assets/slides/photos.svg',
+        img: '/assets/slides/semana37.png',
         titulo: 'Semana 37',
         desc: 'Mira y comparte increíbles fotos de todo el mundo'
       },
       {
-        img: '/assets/slides/music-player-2.svg',
+        img: '/assets/slides/semana38.png',
         titulo: 'Semana 38',
         desc: 'Toda tu música favorita está aquí'
       },
       {
-        img: '/assets/slides/calendar.svg',
+        img: '/assets/slides/semana39.png',
         titulo: 'Semana 39',
         desc: 'El mejor calendario del mundo a tu disposición'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana40.png',
         titulo: 'Semana 40',
         desc: 'Siempre sabremos donde estás!'
       },
       {
-        img: '/assets/slides/placeholder-1.svg',
+        img: '/assets/slides/semana41.png',
         titulo: 'Semana 41',
+        desc: 'Siempre sabremos donde estás!'
+      },
+      {
+        img: '/assets/slides/semana42.png',
+        titulo: 'Semana 42',
         desc: 'Siempre sabremos donde estás!'
       }
 ]
